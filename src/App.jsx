9c47d5e213e33fb62bb9abce9e9c20cfa8088dd3
H-M-Nizum <hmnizum1714032@gmail.com>
2025-03-29 23:58:03 +0600

    Install and connect daisy ui

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,11 @@ function App() {
   return (
     <>
       <h1 className="text-5xl font-bold underline">Vite + React</h1>
+      <button className="btn btn-xs">Xsmall</button>
+      <button className="btn btn-sm">Small</button>
+      <button className="btn">Medium</button>
+      <button className="btn btn-lg">Large</button>
+      <button className="btn btn-xl">Xlarge</button>
     </>
   );
 }
